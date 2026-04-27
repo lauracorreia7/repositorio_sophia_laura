d12159bf29423afed75a58dbe46e525dc76fd9b8
Sophia <sophiavg@al.insper.edu.br>
2026-04-27 18:17:20 -0300

    2

--- funcoes.py
+++ funcoes.py
@@ -4,3 +4,15 @@ def rolar_dados(vezes):
     for i in range(vezes):
         lista_dados.append(random.randint(1,6))
     return lista_dados
+
+def guardar_dado(dados_rolados1, dados_guardados, indice):
+    dados_rolados2 = []
+    for i in range(len(dados_rolados1)):
+        if i == indice:
+            dados_guardados.append(dados_rolados1[i])
+        else:
+            dados_rolados2.append(dados_rolados1[i])
+    lista = []
+    lista.append(dados_rolados2)
+    lista.append(dados_guardados)
+    return (lista)
