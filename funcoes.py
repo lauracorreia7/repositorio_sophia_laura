3ab1df5264f7742031d471712cd0263fe320d81c
Laura Correia <lauracaratincorreia@gmail.com>
2026-04-28 07:59:41 -0300

    regra simples + soma pontos

--- funcoes.py
+++ funcoes.py
@@ -28,3 +28,26 @@ def remover_dado(dados_rolados, dados_guardados, indice):
     lista.append(dados_rolados)
     lista.append(dados_guardados2)
     return (lista)
+
+def calcula_pontos_regra_simples(faces):
+    dicio = {1:0,2:0,3:0,4:0,5:0,6:0}
+    for face in faces:
+        if face==1:
+            dicio[1]=dicio[1]+1
+        if face==2:
+            dicio[2]=dicio[2]+2
+        if face==3:
+            dicio[3]=dicio[3]+3
+        if face==4:
+            dicio[4]=dicio[4]+4
+        if face==5:
+            dicio[5]=dicio[5]+5
+        if face==6:
+            dicio[6]=dicio[6]+6
+    return dicio
+
+def calcula_pontos_soma(faces):
+    soma = 0
+    for lado in faces:
+        soma = soma + lado
+    return soma
